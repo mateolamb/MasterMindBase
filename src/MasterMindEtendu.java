91e5b790c46f32ac579fa1f052a516026484eb16
gf <gliglidu34@gmail.com>
2022-11-26 12:38:28 +0100

    modifiacation du base

--- src/MasterMindEtendu.java
+++ src/MasterMindEtendu.java
@@ -449,11 +449,12 @@ public class MasterMindEtendu {
 
     //___________________________________________________________________
 
-    /**
-     * pré-requis : les éléments de cod1 sont des entiers de 0 à nbCouleurs-1
-     * action : met dans cod1 le code qui le suit selon l'ordre lexicographique dans l'ensemble
-     * des codes de longueur cod1.length à valeurs de 0 à nbCouleurs-1, si ce code existe
-     * résultat : vrai ssi l'action a pu être effectuée
+    /**CHANGE : action si le code suivant n'existe pas
+     *************************************************
+     pré-requis : les éléments de cod1 sont des entiers de 0 à nbCouleurs-1
+     action/résultat : met dans cod1 le code qui le suit selon l'ordre lexicographique (dans l'ensemble
+     des codes à valeurs  de 0 à nbCouleurs-1) et retourne vrai si ce code existe,
+     sinon met dans cod1 le code ne contenant que des "0" et retourne faux
      */
     public static boolean passeCodeSuivantLexico(int[] cod1, int nbCouleurs) {
 
@@ -464,26 +465,34 @@ public class MasterMindEtendu {
             } else if (cod1[i] == nbCouleurs - 1) {
                 cod1[i] = 0;
             } else {
+                for (int j = 0; j < cod1.length; j++) {
+                    cod1[j]=0;
+                }
                 return false;
             }
+        }
+        for (int j = 0; j < cod1.length; j++) {
+            cod1[j]=0;
         }
         return false;
     }
 
     //___________________________________________________________________
 
-    /**
-     * pré-requis : cod est une matrice, rep est une matrice à 2 colonnes, 0 <= nbCoups < cod.length
-     * et  nbCoups < rep.length
-     * résultat : vrai ssi cod[nbCoups] est compatible avec les nbCoups premières lignes de cod et de rep,
-     * c'est-à-dire que si cod[nbCoups] était le code secret, les réponses aux nbCoups premières
-     * propositions de cod seraient les nbCoups premières réponses de rep
+    /**CHANGE : ajout du paramètre cod1 et modification des spécifications
+     *********************************************************************
+     pré-requis : cod est une matrice à cod1.length colonnes, rep est une matrice à 2 colonnes, 0 <= nbCoups < cod.length,
+     nbCoups < rep.length et les éléments de cod1 et de cod sont des entiers de 0 à nbCouleurs-1
+     résultat : vrai ssi cod1 est compatible avec les nbCoups premières lignes de cod et de rep,
+     c'est-à-dire que si cod1 était le code secret, les réponses aux nbCoups premières
+     propositions de cod seraient les nbCoups premières réponses de rep resp.
      */
-    public static boolean estCompat(int[][] cod, int[][] rep, int nbCoups, int nbCouleurs) {
+
+    public static boolean estCompat(int [] cod1, int [][] cod,int [][] rep, int nbCoups, int  nbCouleurs){
 
         for (int i = 0; i < nbCoups; i++) {
 
-            int[] nbBienMalPlaces = nbBienMalPlaces(cod[i], cod[nbCoups], nbCouleurs);
+            int[] nbBienMalPlaces = nbBienMalPlaces(cod[i], cod1, nbCouleurs);
             if (!sontEgaux(nbBienMalPlaces, rep[i])) {
                 return false;
             }
@@ -494,20 +503,21 @@ public class MasterMindEtendu {
 
     //___________________________________________________________________
 
-    /**
-     * pré-requis : cod est une matrice, rep est une matrice à 2 colonnes, 0 < nbCoups < cod.length
-     * et nbCoups < rep.length
-     * action : met dans cod[nbCoups] le plus petit code (selon l'ordre lexicographique dans l'ensemble
-     * des codes de longueur cod[0].length à valeurs  de 0 à nbCouleurs-1) qui est à la fois plus grand que
-     * cod[nbCoups-1] selon cet ordre et compatible avec les nbCoups premières lignes de cod et de rep,
-     * si ce code existe
-     * résultat : vrai ssi l'action a pu être effectuée
+    /**CHANGE : renommage de passePropSuivante en passeCodeSuivantLexicoCompat,
+     ajout du paramètre cod1 et modification des spécifications
+     **************************************************************************
+     pré-requis : cod est une matrice à cod1.length colonnes, rep est une matrice à 2 colonnes, 0 <= nbCoups < cod.length,
+     nbCoups < rep.length et les éléments de cod1 et de cod sont des entiers de 0 à nbCouleurs-1
+     action/résultat : met dans cod1 le plus petit code (selon l'ordre lexicographique (dans l'ensemble
+     des codes à valeurs  de 0 à nbCouleurs-1) qui est à la fois plus grand que
+     cod1 selon cet ordre et compatible avec les nbCoups premières lignes de cod et rep si ce code existe,
+     sinon met dans cod1 le code ne contenant que des "0" et retourne faux
      */
-    public static boolean passePropSuivante(int[][] cod, int[][] rep, int nbCoups, int nbCouleurs) {
+    public static boolean passeCodeSuivantLexicoCompat(int [] cod1, int [][] cod,int [][] rep, int nbCoups, int  nbCouleurs){
         cod[nbCoups] = copieTab(cod[nbCoups - 1]);
 
         while (passeCodeSuivantLexico(cod[nbCoups], nbCouleurs)) {
-            if (estCompat(cod, rep, nbCoups, nbCouleurs)) {
+            if (estCompat(cod1, cod, rep, nbCoups, nbCouleurs)) {
                 return true;
             }
         }
@@ -547,7 +557,7 @@ public class MasterMindEtendu {
         }
 
         for (int i = 1; i < nbEssaisMax; i++) {
-            if (!passePropSuivante(sauvegardeCode, sauvegardeRep, i, tabCouleurs.length)) {
+            if (!passeCodeSuivantLexicoCompat(sauvegardeCode, sauvegardeRep, i, tabCouleurs.length)) {
 
                 System.out.println("Vous vous êtes trompé dans vos saisies.");
                 System.out.println("\n------------------------------\n");
@@ -797,14 +807,16 @@ public class MasterMindEtendu {
     //.........................................................................
 
 
-    /**
-     * action : demande à l'utilisateur de saisir les paramètres de la partie (lgCode, tabCouleurs,
-     * nbManches, nbEssaisMax),
-     * effectue la partie et affiche le résultat (identité du gagnant ou match nul).
-     * La longueur d'un code et le nombre de couleurs doivent être strictement positifs.
-     * Le nombre de manches doit être un nombre pair strictement positif.
-     * Les initiales des noms de couleurs doivent être différentes.
-     * Toute donnée incorrecte doit être re-saisie jusqu'à ce qu'elle soit correcte.
+
+    /**CHANGE : ajout de : le nombre d'essais maximum doit être strictement positif
+     ******************************************************************************
+     action : demande à l'utilisateur de saisir les paramètres de la partie (lgCode, tabCouleurs,
+     nbManches, nbEssaisMax),
+     effectue la partie et affiche le résultat (identité du gagnant ou match nul).
+     La longueur d'un code, le nombre de couleurs et le nombre d'essais maximum doivent être strictement positifs.
+     Le nombre de manches doit être un nombre pair strictement positif.
+     Les initiales des noms de couleurs doivent être différentes.
+     Toute donnée incorrecte doit être re-saisie jusqu'à ce qu'elle soit correcte.
      */
     public static void main(String[] args) {
 
